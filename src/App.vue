a00f32953b28f437fc74ebb56f38f5a18e55c962
sabbunsopum <sabbunsopum@gmail.com>
2022-11-08 16:31:31 +0900

    221108

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <HeaderLayout fonts="nexon" />
+    <SliderSection />
+    <ImageSection attr="section nexon" layout="container" />
+    <ImgTextSection attr="nexon section gray" layout="container" />
+    <CardSection attr="section nexon" layout="container" />
+    <BannerSection attr="nexon" />
+    <TextSection attr="section nexon" layout="container" />
+    <FooterLayout attr="section nexon gray" layout="container" />
+  </div>
+</template>
+
+<script>
+import FooterLayout from "./components/layout/FooterLayout.vue";
+import HeaderLayout from "./components/layout/HeaderLayout.vue";
+import BannerSection from "./components/section/BannerSection.vue";
+import CardSection from "./components/section/CardSection.vue";
+import ImageSection from "./components/section/ImageSection.vue";
+import ImgTextSection from "./components/section/ImgTextSection.vue";
+import SliderSection from "./components/section/SliderSection.vue";
+import TextSection from "./components/section/TextSection.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderLayout,
+    FooterLayout,
+    SliderSection,
+    ImageSection,
+    ImgTextSection,
+    CardSection,
+    BannerSection,
+    TextSection,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/section/BannerSection.vue
@@ -0,0 +1,79 @@
+<template>
+  <section
+    id="bannerType"
+    class="banner__wrap content__item scroll"
+    :class="attr"
+  >
+    <h2 class="blind">Ask</h2>
+    <div class="banner__inner">
+      <h3 class="title">Ask</h3>
+      <p class="desc">
+        궁금한 점이 있으신가요? 아래 이메일로 연락 주세요!
+        <a href="#" title="새 페이지 열림">D_Deco@abcde.com</a>
+      </p>
+      <span class="small">고객 센터</span>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    attr: String,
+    layout: String,
+  },
+};
+</script>
+
+<style scope>
+.banner__inner {
+  background-image: url(../../assets/img/banner_bg01.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  text-align: center;
+  padding: 120px 0;
+  color: #fff;
+}
+.banner__inner .title {
+  font-size: 50px;
+  line-height: 1;
+  font-weight: 300;
+  margin-bottom: 40px;
+}
+.banner__inner .desc {
+  font-size: 24px;
+  line-height: 1.5;
+  font-weight: 300;
+  margin-bottom: 70px;
+}
+.banner__inner .desc a {
+  color: #fff;
+  display: block;
+}
+.banner__inner .desc a:hover {
+  text-decoration: underline;
+}
+.banner__inner .small {
+  font-size: 16px;
+  text-decoration: underline;
+}
+
+@media (max-width: 960px) {
+  .banner__inner {
+    padding: 100px 0;
+  }
+}
+@media (max-width: 600px) {
+  .banner__inner {
+    padding: 80px 0;
+  }
+  .banner__inner .title {
+    font-size: 30px;
+    margin-bottom: 20px;
+  }
+  .banner__inner .desc {
+    font-size: 16px;
+    margin-bottom: 40px;
+  }
+}
+</style>
